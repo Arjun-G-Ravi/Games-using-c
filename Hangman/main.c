6f047d9f56a85d93f0d77d1229805d16088e96e4
Arjun <arjungravi1@gmail.com>
2023-12-16 20:33:18 +0530

    Commited

---
+++ Hangman/main.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main(){
+    char names[1000]
+    FILE *fptr = fopen("names.txt","r");
+
+    
+    fclose(fptr); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
