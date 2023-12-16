417967fc5a1a237a16c8d2b151f8f5ee3ae72cf8
Arjun <arjungravi1@gmail.com>
2023-12-16 13:25:21 +0530

    Commited

--- Rock-Paper-Scissors/main.c
+++ Rock-Paper-Scissors/main.c
@@ -1,1 +1,22 @@
-# include <
+# include<stdio.h>
+
+int main(){
+    int playerScore = 0;
+    int compScore = 0;
+    int currMove = 0;
+
+    printf("You can choose ROCK, PAPER or SCISSORS. The first player to score 10 wins.");
+
+    while(playerScore < 10 && compScore < 10){
+        // Player's move
+        printf("What do you choose:\n1. Rock\n2. Paper\n3.Scissors\nType 1, 2 or 3: \n");
+        scanf("%d", &currMove);
+
+        // Computer's move
+
+    }
+
+
+
+    return 0;
+}
